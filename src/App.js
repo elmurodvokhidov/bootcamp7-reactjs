9389f77e487fd8e7697097eba1817be753d03c1d
elmurod <elmurodvkh@gmail.com>
2023-11-29 22:03:45 +0500

    lesson1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './Main.css';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
+  function inc() {
+    setCount(prev => prev + 1)
+  }
+
+  function dec() {
+    if (count > 0) {
+      setCount(prev => prev - 1)
+    }
+  }
+
+  let btnStyle = {
+    border: '1px solid black',
+    backgroundColor: 'red',
+    fontSize: '30px',
+    padding: '10px 20px',
+    color: 'white'
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ color: 'red', fontSize: '50px' }}>{count}</h1>
+      <button style={btnStyle} onClick={inc}>inc</button>
+      <button onClick={dec}>dec</button>
+      <img src="./img/p3563021_b_v8_bb.jpg" alt="" />
     </div>
   );
 }
