fd115cfd03b5a603b20a1f4c59e4bf88900333b1
elmurod <elmurodvkh@gmail.com>
2023-12-25 16:46:20 +0500

    lesson10 - promocode-subtotal

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -14,7 +14,7 @@ function Basket() {
     } = useContext(ContextData);
 
     return (
-        <div className="home container py-5">
+        <div className="home container py-5 basket">
             <div className="scrollBar">
                 <Table striped className="text-center">
                     <thead>
@@ -30,27 +30,53 @@ function Basket() {
                     </thead>
                     <tbody>
                         {
-                            basket.map((item, index) => (
-                                <tr key={item.id}>
-                                    <td>{index + 1}</td>
-                                    <td><img style={{ width: "120px" }} src={item.img} alt={item.title} /></td>
-                                    <td>{item.title}</td>
-                                    <td>{item.description.length > 20 ? item.description.slice(0, 20) + "..." : item.description}</td>
-                                    <td>{calcDis(item.price, item.discount) * item.count}$</td>
-                                    <td>
-                                        <div className="d-flex flex-row justify-content-center align-items-center gap-3">
-                                            <button onClick={() => decrement(item)} className="btn btn-secondary"><FaMinus /></button>
-                                            <span>{item.count}</span>
-                                            <button onClick={() => increment(item)} className="btn btn-success"><FaPlus /></button>
-                                        </div>
-                                    </td>
-                                    <td><button onClick={() => deleteProductFromCart(item.id)} className="btn btn-danger"><MdDelete /></button></td>
-                                </tr>
-                            ))
+                            basket.length > 0 ?
+                                basket.map((item, index) => (
+                                    <tr key={item.id}>
+                                        <td>{index + 1}</td>
+                                        <td><img style={{ width: "120px" }} src={item.img} alt={item.title} /></td>
+                                        <td>{item.title}</td>
+                                        <td>{item.description.length > 20 ? item.description.slice(0, 20) + "..." : item.description}</td>
+                                        <td>{calcDis(item.price, item.discount) * item.count}$</td>
+                                        <td>
+                                            <div className="d-flex flex-row justify-content-center align-items-center gap-3">
+                                                <button onClick={() => decrement(item)} className="btn btn-secondary"><FaMinus /></button>
+                                                <span>{item.count}</span>
+                                                <button onClick={() => increment(item)} className="btn btn-success"><FaPlus /></button>
+                                            </div>
+                                        </td>
+                                        <td><button onClick={() => deleteProductFromCart(item.id)} className="btn btn-danger"><MdDelete /></button></td>
+                                    </tr>
+                                )) : <tr style={{ height: "380px" }}><td colSpan={10} style={{ color: "red", fontSize: "25px" }}>Savatda mahsulot topilmadi!</td></tr>
                         }
                     </tbody>
                 </Table>
             </div>
+
+            <footer className="d-flex align-items-start justify-content-between">
+                <div className="promocode">
+                    <input className="py-2 px-3" type="text" name="promocode" id="promocode" placeholder="Enter your promocode..." />
+                    <label className="btn btn-dark" htmlFor="promocode">Enter</label>
+                </div>
+
+                <div className="subtotal">
+                    <div className="d-flex flex-column gap-2">
+                        <div className="item">
+                            <h3>Jami:</h3>
+                            <h3>{basket.length > 0 ? basket.reduce((amount, item) => amount + (calcDis(item.price, item.discount) * item.count), 0) : "0"}$</h3>
+                        </div>
+                        <div className="item">
+                            <h5>Promocode:</h5>
+                            <h5>No</h5>
+                        </div>
+                        <div className="item">
+                            <h5>Delivery:</h5>
+                            <h5>0$</h5>
+                        </div>
+                    </div>
+                    <button className="btn btn-dark w-100 mt-4 p-2">Buyurtma berish</button>
+                </div>
+            </footer>
         </div>
     )
 }
--- src/pages/Likes.js
+++ src/pages/Likes.js
@@ -7,13 +7,13 @@ function Likes() {
 
     return (
         <div className="home container py-5">
-            <h1>Barcha yoqtirilgan mahsulotlar</h1>
+            <h2>Barcha yoqtirilgan mahsulotlar</h2>
 
             <div className="row row-cols-1 row-cols-md-4 gap-5 justify-content-left pb-4 pt-3 my-5">
                 {
-                    like.map(item => (
+                    like.length > 0 ? like.map(item => (
                         <CardComponent item={item} key={item.id} />
-                    ))
+                    )) : <h4 className="text-center w-100 my-5 text-danger">Yoqtirilgan mahsulot yo'q!</h4>
                 }
             </div>
         </div>
